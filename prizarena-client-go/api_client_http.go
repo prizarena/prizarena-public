2eff49dfeb91cc9f4978ab9cb765eb8fa4977358
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2018-06-05 09:37:41 +0100

    errors handling prototype

--- prizarena-client-go/api_client_http.go
+++ prizarena-client-go/api_client_http.go
@@ -5,11 +5,14 @@ import (
 	"context"
 	"github.com/prizarena/prizarena-public/prizarena-interfaces"
 	"strings"
+	"io"
 	"encoding/json"
+	"github.com/pkg/errors"
+	"fmt"
 )
 
 const (
-	contentTypeJson = "application/json"
+	contentTypeJson          = "application/json"
 	ApiEndpointNewTournament = "/api/new-tournament"
 	ApiEndpointPlayCompleted = "/api/play-completed"
 )
@@ -20,29 +23,55 @@ func NewHttpApiClient(httpClient *http.Client, token string) prizarena_interface
 
 type httpApiClient struct {
 	httpClient *http.Client
-	token string
+	token      string
 }
 
-func (apiClient httpApiClient) NewTournament(c context.Context, newTournament prizarena_interfaces.NewTournament) (tournament prizarena_interfaces.Tournament, err error) {
-	body := strings.Reader{}
+func (apiClient httpApiClient) post(endpoint string, body io.Reader, response interface{}) (err error) {
 	var resp *http.Response
-	resp, err = apiClient.httpClient.Post(ApiEndpointNewTournament, contentTypeJson, &body)
-	if err != nil {
+	if resp, err = apiClient.httpClient.Post(endpoint, contentTypeJson, &body); err != nil {
 		return
 	}
 	defer resp.Body.Close()
 	switch resp.StatusCode {
 	case http.StatusOK:
-		err = json.NewDecoder(resp.Body).Decode(&tournament)
+		if err = json.NewDecoder(resp.Body).Decode(response); err != nil {
+			err = errors.WithMessage(err, "failed to decode response with OK status code as JSON")
+			return
+		}
+	case http.StatusUnauthorized:
+		err = ErrUnauthorized;
+		return
+	case http.StatusForbidden:
+		err = ErrForbidden;
+		return
 	case http.StatusInternalServerError, http.StatusBadRequest:
 		errResp := ErrorResponse{}
-		err = json.NewDecoder(resp.Body).Decode(&errResp)
+		if err = json.NewDecoder(resp.Body).Decode(&errResp); err != nil {
+			err = errors.WithMessage(err, fmt.Sprintf("failed to decode response with status code %v as JSON", resp.StatusCode))
+			return
+		}
+		switch resp.StatusCode {
+		case http.StatusBadRequest:
+			err = ErrBadRequest{apiError: apiError{Code: errResp.Code, Message: errResp.Message}}
+			return
+		case http.StatusInternalServerError:
+			err = ErrInternalServerError{apiError: apiError{Code: errResp.Code, Message: errResp.Message}}
+			return
+		default:
+			err = apiError{Code: errResp.Code, Message: errResp.Message}
+		}
 	}
 	return
 }
 
-func (apiClient httpApiClient) PlayCompleted(c context.Context, e prizarena_interfaces.PlayCompletedEvent) (err error){
+func (apiClient httpApiClient) NewTournament(c context.Context, newTournament prizarena_interfaces.NewTournament) (tournament prizarena_interfaces.Tournament, err error) {
 	body := strings.Reader{}
-	_, err = apiClient.httpClient.Post(ApiEndpointPlayCompleted, contentTypeJson, &body)
+	err = apiClient.post(ApiEndpointNewTournament, &body, &tournament)
+	return
+}
+
+func (apiClient httpApiClient) PlayCompleted(c context.Context, e prizarena_interfaces.PlayCompletedEvent) (err error) {
+	body := strings.Reader{}
+	err = apiClient.post(ApiEndpointPlayCompleted, &body, &PlayCompletedResponse{})
 	return
 }
--- prizarena-client-go/responses.go
+++ prizarena-client-go/responses.go
@@ -1,7 +1,18 @@
 package prizarena
 
+import "github.com/prizarena/prizarena-public/prizarena-interfaces"
+
+//go:generate ffjson $GOFILE
+
 type ErrorResponse struct {
 	Code string
 	Message string
 }
 
+type TournamentCompletedResponse struct {
+	Tournament prizarena_interfaces.Tournament
+}
+
+type PlayCompletedResponse struct {
+
+}
