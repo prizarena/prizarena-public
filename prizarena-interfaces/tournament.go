753c0afbbb01e7f03674e2b2b56d65014c7fc652
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2018-06-12 07:36:13 +0100

    refactoring

--- prizarena-interfaces/tournament.go
+++ prizarena-interfaces/tournament.go
@@ -1,5 +1,10 @@
 package prizarena_interfaces
 
+import (
+	"strings"
+	"strconv"
+)
+
 const MonthlyTournamentIDFormat = "200601"
 
 type TournamentDto struct {
@@ -11,4 +16,16 @@ type TournamentDto struct {
 
 type NewTournamentResponse struct {
 	Tournament TournamentDto
+}
+
+func IsMonthlyTournamentID(tournamentID string) bool {
+	if len(tournamentID) <= 7 || strings.Count(tournamentID, ":") != 1 {
+		return false
+	}
+	if tournamentID = strings.Split(tournamentID, ":")[1]; len(tournamentID) == 6 {
+		if v, err := strconv.ParseInt(tournamentID, 10, 32); err == nil && v > 201801 {
+			return true
+		}
+	}
+	return false
 }
