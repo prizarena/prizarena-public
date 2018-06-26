5ba84cfb46ebf5727d350f8211127ca32f35fa5f
Alexander Trakhimenok <alexander.trakhimenok@gmail.com>
2018-06-26 08:13:42 +0100

    Start callback fixed

--- pabot/pap-cmd-onstart-tournament.go
+++ pabot/pap-cmd-onstart-tournament.go
@@ -3,7 +3,7 @@ package pabot
 import (
 	"github.com/strongo/bots-framework/core"
 	"strings"
-			"github.com/prizarena/prizarena-public/pamodels"
+	"github.com/prizarena/prizarena-public/pamodels"
 )
 
 const onStartTournamentCode = "PapOnStartTournament"
@@ -16,6 +16,9 @@ var OnStartTournament = bots.Command{
 }
 
 func OnStartIfTournamentLink(whc bots.WebhookContext, gameID string) (m bots.MessageFromBot, err error) {
+	if whc.InputType() != bots.WebhookInputText {
+		return
+	}
 	input := whc.Input().(bots.WebhookTextMessage)
 	text := input.Text()
 	if strings.HasPrefix(text, "/start ") {
